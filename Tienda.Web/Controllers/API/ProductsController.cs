02049c1a9c76e0410e4d00d2114ff90e7b6843ab
x905 <angel.fsociety@gmail.com>
2020-02-14 17:29:45 -0600

    Products Api Controller

---
+++ Tienda.Web/Controllers/API/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Tienda.Web.Controllers.API
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Tienda.Web.Data;
+
+    [Route("api/[Controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(this.productRepository.GetAll());
+        }
+
+
+    }
+}
